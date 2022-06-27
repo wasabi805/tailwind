68cf1f83ca3fa87dbb9b6a5adb8a8ad3b2880c2b
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-06-26 18:20:25 -0700

    wired up onSubmit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,31 @@
 console.log("i work");
 
-const users =[{
-    firstName: 'Matt',
-    lastName : 'Murdock',
-    addressLine: '1234 Hell Kitchen Dr',
-    city: 'New York',
-    state:'NY',
-    zipcode:'12345',
-}]
+const users = [
+  {
+    firstName: "Matt",
+    lastName: "Murdock",
+    addressLine: "1234 Hells Kitchen Dr",
+    city: "New York",
+    state: "NY",
+    zipcode: "12345",
+  },
+];
 
-module.exports={users}
+const otherUsers = [
+  {
+    firstName: "Marc",
+    lastName: "Spector",
+    addressLine: "4563 Hells Kitchen Dr",
+    city: "New York",
+    state: "NY",
+    zipcode: "12345",
+  },
+];
+
+function handleSubmit() {
+  const userForm = document.getElementById("userForm");
+
+  for (var i = 0; i < Object.entries(userForm).length; i++) {
+    console.log("what is i", userForm[i].value);
+  }
+}
